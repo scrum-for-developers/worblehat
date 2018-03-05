402ee510d52c63a1bffb99647ed0e0a98a4b095b
Holger Grosse-Plankermann <holgergp@gmail.com>
2018-03-05 13:26:21 +0100

    Change without reformatting

--- worblehat-domain/src/main/java/de/codecentric/psd/worblehat/domain/StandardBookService.java
+++ worblehat-domain/src/main/java/de/codecentric/psd/worblehat/domain/StandardBookService.java
@@ -1,14 +1,13 @@
 package de.codecentric.psd.worblehat.domain;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import javax.annotation.Nonnull;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * The domain service class for book operations.
@@ -17,85 +16,85 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class StandardBookService implements BookService {
 
-    public StandardBookService() {
+	public StandardBookService() {
 
-    }
+	}
 
-    @Autowired
-    public StandardBookService(BorrowingRepository borrowingRepository, BookRepository bookRepository) {
-        this.borrowingRepository = borrowingRepository;
-        this.bookRepository = bookRepository;
-    }
+	@Autowired
+	public StandardBookService(BorrowingRepository borrowingRepository, BookRepository bookRepository) {
+		this.borrowingRepository = borrowingRepository;
+		this.bookRepository = bookRepository;
+	}
 
-    private BorrowingRepository borrowingRepository;
+	private BorrowingRepository borrowingRepository;
 
-    private BookRepository bookRepository;
+	private BookRepository bookRepository;
 
-    @Override
-    public void returnAllBooksByBorrower(String borrowerEmailAddress) {
-        List<Borrowing> borrowingsByUser = borrowingRepository
-                .findBorrowingsByBorrower(borrowerEmailAddress);
-        for (Borrowing borrowing : borrowingsByUser) {
-            borrowingRepository.delete(borrowing);
-        }
-    }
+	@Override
+	public void returnAllBooksByBorrower(String borrowerEmailAddress) {
+		List<Borrowing> borrowingsByUser = borrowingRepository
+				.findBorrowingsByBorrower(borrowerEmailAddress);
+		for (Borrowing borrowing : borrowingsByUser) {
+			borrowingRepository.delete(borrowing);
+		}
+	}
 
-    @Override
-    public Optional<Borrowing> borrowBook(String isbn, String borrower) {
-        Set<Book> books = bookRepository.findByIsbn(isbn);
+	@Override
+	public Optional<Borrowing> borrowBook(String isbn, String borrower) {
+		Set<Book> books = bookRepository.findByIsbn(isbn);
 
-        Optional<Book> unborrowedBook = books.stream()
-                .filter(book -> book.getBorrowing() == null)
-                .findFirst();
+		Optional<Book> unborrowedBook = books.stream()
+				.filter(book -> book.getBorrowing() == null)
+				.findFirst();
 
-        return unborrowedBook.map(book -> {
-            book.borrowNowByBorrower(borrower);
-            borrowingRepository.save(book.getBorrowing());
-            return book.getBorrowing();
-        });
-    }
+		return unborrowedBook.map(book -> {
+			book.borrowNowByBorrower(borrower);
+			borrowingRepository.save(book.getBorrowing());
+			return book.getBorrowing();
+		});
+	}
 
-    @Override
-    public Set<Book> findBooksByIsbn(String isbn) {
-        return bookRepository.findByIsbn(isbn); //null if not found
-    }
+	@Override
+	public Set<Book> findBooksByIsbn(String isbn) {
+		return bookRepository.findByIsbn(isbn); //null if not found
+	}
 
-    @Override
-    public List<Book> findAllBooks() {
-        return bookRepository.findAllByOrderByTitle();
-    }
+	@Override
+	public List<Book> findAllBooks() {
+		return bookRepository.findAllByOrderByTitle();
+	}
 
 
-    @Override
-    public Optional<Book> createBook(@Nonnull String title,
-                                     @Nonnull String author,
-                                     @Nonnull String edition,
-                                     @Nonnull String isbn,
-                                     int yearOfPublication) {
-        Book book = new Book(title, author, edition, isbn, yearOfPublication);
+	@Override
+	public Optional<Book> createBook(@Nonnull String title,
+									 @Nonnull String author,
+									 @Nonnull String edition,
+									 @Nonnull String isbn,
+									 int yearOfPublication) {
+		Book book = new Book(title, author, edition, isbn, yearOfPublication);
 
-        Optional<Book> bookFromRepo = bookRepository.findTopByIsbn(isbn);
+		Optional<Book> bookFromRepo = bookRepository.findTopByIsbn(isbn);
 
-        Book newBook = bookFromRepo.orElseGet(() -> bookRepository.save(book));
+		Book newBook = bookFromRepo.orElseGet(() -> bookRepository.save(book));
 
-        Optional<Book> exisitingBookDifferentEdition = bookFromRepo
-                .filter(b -> b.isSameCopy(book))
-                .map(b -> bookRepository.save(b));
+		Optional<Book> exisitingBookDifferentEdition = bookFromRepo
+				.filter(b -> b.isSameCopy(book))
+				.map(b -> bookRepository.save(b));
 
-        return Optional.of(exisitingBookDifferentEdition).orElse(Optional.of(newBook));
-    }
+		return Optional.of(exisitingBookDifferentEdition).orElse(Optional.of(newBook));
+	}
 
-    @Override
-    public boolean bookExists(String isbn) {
-        Set<Book> books = bookRepository.findByIsbn(isbn);
-        return !books.isEmpty();
-    }
+	@Override
+	public boolean bookExists(String isbn) {
+		Set<Book> books = bookRepository.findByIsbn(isbn);
+		return !books.isEmpty();
+	}
 
-    @Override
-    public void deleteAllBooks() {
-        borrowingRepository.deleteAll();
-        bookRepository.deleteAll();
-    }
+	@Override
+	public void deleteAllBooks() {
+		borrowingRepository.deleteAll();
+		bookRepository.deleteAll();
+	}
 
 
 }
