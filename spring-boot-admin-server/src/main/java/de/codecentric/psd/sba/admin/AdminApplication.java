1ff4b512f5268b98b8ffc704ef1b509a2c166f52
Andreas Ebbert-Karroum <andreas@karroum.de>
2020-02-27 12:06:38 +0100

    Spring Boot project, generated with start.spring.io, for Spring Boot Admin

---
+++ spring-boot-admin-server/src/main/java/de/codecentric/psd/sba/admin/AdminApplication.java
@@ -0,0 +1,13 @@
+package de.codecentric.psd.sba.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+}
