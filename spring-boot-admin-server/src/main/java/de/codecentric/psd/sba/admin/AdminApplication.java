f9ba738cd4749644342d729af855a156d49e245d
Andreas Ebbert-Karroum <andreas@karroum.de>
2020-02-27 12:21:07 +0100

    Add Spring Boot Admin on port 9100

--- spring-boot-admin-server/src/main/java/de/codecentric/psd/sba/admin/AdminApplication.java
+++ spring-boot-admin-server/src/main/java/de/codecentric/psd/sba/admin/AdminApplication.java
@@ -2,8 +2,14 @@ package de.codecentric.psd.sba.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class AdminApplication {
 
 	public static void main(String[] args) {
