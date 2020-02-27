0b79eb03f99a0f5d7dedd919f40b6bc6c1a7d4aa
Andreas Ebbert-Karroum <andreas@karroum.de>
2020-02-27 15:41:20 +0100

    remove spring boot admin server, add sba client dependencies, to be activated on deployment

--- worblehat-web/src/main/java/de/codecentric/psd/Worblehat.java
+++ worblehat-web/src/main/java/de/codecentric/psd/Worblehat.java
@@ -3,17 +3,28 @@ package de.codecentric.psd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import java.util.Scanner;
 
 @SpringBootApplication
 public class Worblehat {
 
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+		}
+	}
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(Worblehat.class, args);
 
-		// this code is basically to (a) demonstrate how to stop a Spring application and (b)
+		// this code is basically to (a) demonstrate how to stop a Spring application
+		// and (b)
 		// get rid of the SonarQube warning to close the context properly
 		System.out.println("Enter 'stop' to stop Worblehat.");
 		String line = "";
