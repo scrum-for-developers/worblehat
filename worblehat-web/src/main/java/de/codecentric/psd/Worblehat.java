d32b6d43ce73f5a3aeed1210f537136083c710b8
Andreas Ebbert-Karroum <andreas@karroum.de>
2021-03-12 09:14:47 +0100

    Simplify main method. Apparently, that SonarQube warning that complains about an unclosed context doesn't exist any longer?

--- worblehat-web/src/main/java/de/codecentric/psd/Worblehat.java
+++ worblehat-web/src/main/java/de/codecentric/psd/Worblehat.java
@@ -1,11 +1,7 @@
 package de.codecentric.psd;
 
-import java.util.Scanner;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,7 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @SpringBootApplication
 public class Worblehat {
 
-  private static final Logger LOG = LoggerFactory.getLogger(Worblehat.class);
+  public static void main(String[] args) {
+    SpringApplication.run(Worblehat.class, args);
+  }
 
   @Configuration
   public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
@@ -23,19 +21,4 @@ public class Worblehat {
     }
   }
 
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    ConfigurableApplicationContext applicationContext =
-        SpringApplication.run(Worblehat.class, args);
-
-    // this code is basically to (a) demonstrate how to stop a Spring application
-    // and (b)
-    // get rid of the SonarQube warning to close the context properly
-    LOG.info("Enter 'stop' to stop Worblehat.");
-    String line = "";
-    do {
-      line = scan.nextLine();
-    } while (!line.equals("stop"));
-    applicationContext.close();
-  }
 }
