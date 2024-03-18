a7f31cde88d2c3255a4c29b202e810f9efea9db1
Andreas Ebbert-Karroum <andreas@karroum.de>
2024-03-18 16:23:51 +0100

    Cucumber with JUnit 5

--- worblehat-acceptancetests/src/test/java/de/codecentric/psd/worblehat/AcceptanceTestsIT.java
+++ worblehat-acceptancetests/src/test/java/de/codecentric/psd/worblehat/AcceptanceTestsIT.java
@@ -1,16 +1,20 @@
 package de.codecentric.psd.worblehat;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-    stepNotifications = true,
-    plugin = {
-      "pretty",
-      "html:target/cucumber/index.html",
-      "junit:target/cucumber.xml",
-      "json:target/cucumber-report.json"
-    })
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("de/codecentric/psd/worblehat/features")
+@ConfigurationParameter(
+    key = PLUGIN_PROPERTY_NAME,
+    value =
+        "pretty, "
+            + "html:target/cucumber/index.html,"
+            + "junit:target/cucumber.xml,"
+            + "json:target/cucumber-report.json")
 public class AcceptanceTestsIT {}
